61a3a326ea5faa8c1e4dc14b63a1f76ba27a18bf
chestnut1044 <wbk1620@gmail.com>
2023-03-03 20:19:19 +0900

    graph : disjoint set

--- graph/disjoint sets better.py
+++ graph/disjoint sets better.py
@@ -3,17 +3,19 @@ def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a < b:
-      parent[b] = a
+        parent[b] = a
     else:
-      parent[a] = b
+        parent[a] = b
 
-## 특정 원소가 속한 집합 찾기
+
+## 경로 단축 사용
 def find_parent(parent, x):
     # 만약 초기화해놨던 다르다면
     # => 루트 노드가 아니라면 루트 노드를 찾을때까지 재귀 호출
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
 
 v, e = map(int, input().split())
 parent = [i for i in range(v+1)]
@@ -24,7 +26,7 @@ for i in range(e):
 
 print("각 원소가 속한 집합 : ", end="")
 for i in range(1, v+1):
-  print(find_parent(parent, i), end=" ")
+    print(find_parent(parent, i), end=" ")
 
 print()
 
