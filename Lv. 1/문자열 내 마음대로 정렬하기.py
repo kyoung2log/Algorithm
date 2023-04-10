2e70aefdc25eca9de88b2fca52e411b023f6c037
chestnut1044 <wbk1620@gmail.com>
2023-04-11 06:42:04 +0900

    문자열 내 마음대로 정렬하기

--- Lv. 1/문자열 내 마음대로 정렬하기.py
+++ Lv. 1/문자열 내 마음대로 정렬하기.py
@@ -1,4 +1,7 @@
+def solution(strings, n):
+    strings.sort()
+    strings.sort(key = lambda x : x[n])
+    return strings
 
 def solution(strings, n):
-    answer = sorted(strings, key = lambda x : x[n-1])
-    return sorted(answer, key = lambda x : x[n])
+    return sorted(sorted(strings), key = lambda x : x[n])
