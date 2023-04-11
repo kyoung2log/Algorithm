3b6a7f75860bfdc9410a8d317e73fc9f3369c83d
chestnut1044 <wbk1620@gmail.com>
2023-04-11 10:00:06 +0900

    콜라츠 추측

---
+++ Lv. 1/콜라츠 추측.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = 0
+    while True:
+        if num == 1:
+            break
+        elif num%2 == 0:
+            num //= 2
+        else:
+            num = num*3 + 1
+        answer += 1
+    return answer if answer < 500 else -1
