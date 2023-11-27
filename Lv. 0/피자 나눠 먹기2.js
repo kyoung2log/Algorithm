ef250d37dc8d1ac6c7070a927aac4406e9462e3d
chestnut1044 <wbk1620@gmail.com>
2023-11-27 23:53:13 +0900

    피자 나눠 먹기2

--- Lv. 0/피자 나눠 먹기2.js
+++ Lv. 0/피자 나눠 먹기2.js
@@ -4,3 +4,16 @@ const lcm = (a, b) => (a * b) / gcd(a, b);
 function solution(n) {
   return lcm(6, n) / 6;
 }
+
+const solution = (n) => {
+  let piece = 6;
+
+  while (true) {
+    if (piece % n === 0) {
+      break;
+    }
+    piece += 6;
+  }
+
+  return piece / 6;
+};
