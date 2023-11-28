15997b3a73c318b197b5381197d0cb9913a72f02
chestnut1044 <wbk1620@gmail.com>
2023-11-29 06:17:01 +0900

    모스부호1

---
+++ Lv. 0/모스부호1.js
@@ -0,0 +1,41 @@
+const morse = {
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+};
+
+function solution(letter) {
+  let answer = '';
+  const splitedLetter = letter.split(' ');
+
+  for (const i of splitedLetter) {
+    answer += morse[i];
+  }
+
+  return answer;
+}
+
+return letter.split(' ').reduce((prev, curr) => prev + morse[curr], '');
