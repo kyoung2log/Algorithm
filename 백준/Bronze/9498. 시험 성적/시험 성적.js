823058685949a6e508613f7b6f05884f9353d548
경이 <devkyoung2@gmail.com>
2024-10-13 03:29:00 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+input = parseInt(input);
+
+if (input >= 90) {
+  console.log('A');
+} else if (input >= 80) {
+  console.log('B');
+} else if (input >= 70) {
+  console.log('C');
+} else if (input >= 60) {
+  console.log('D');
+} else {
+  console.log('F');
+}
