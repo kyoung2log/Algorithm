d882e7b045de9e5fe33dfa72f3398bb4b9bc109a
경이 <devkyoung2@gmail.com>
2024-10-13 04:04:19 +0900

    [Bronze V] Title: A+B - 8, Time: 100 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'Wiki\\input.txt';
+const inputs = fs.readFileSync(path).toString().trim().split('\n');
+
+for (let i = 1; i < inputs.length; i++) {
+  const [a, b] = inputs[i].split(' ').map(Number);
+
+  console.log(`Case #${i}: ${a} + ${b} = ${a + b}`);
+}
