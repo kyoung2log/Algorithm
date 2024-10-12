0abdd43621ab521b7f4e4a29378f35e792429c31
경이 <devkyoung2@gmail.com>
2024-10-13 03:46:48 +0900

    [Silver III] Title: N과 M (5), Time: 288 ms, Memory: 26636 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'Wiki\\input.txt';
+const [nm, inputs] = fs.readFileSync(path).toString().trim().split('\n');
+const [n, m] = nm.split(' ').map((it) => Number(it));
+const numbers = inputs.split(' ').map((it) => Number(it));
+numbers.sort((a, b) => a - b);
+
+let answers = '';
+
+const bt = (current, idx) => {
+  if (idx === m) {
+    const answer = current.join(' ');
+    answers += answer + '\n';
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    const target = numbers[i];
+    if (current.includes(target)) continue;
+    bt([...current, target], idx + 1);
+  }
+};
+
+for (const num of numbers) {
+  bt([num], 1);
+}
+
+console.log(answers);
