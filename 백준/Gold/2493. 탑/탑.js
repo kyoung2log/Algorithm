9db14e1627f07ce7a80e73872b7ca15a857bbb7a
경이 <devkyoung2@gmail.com>
2024-10-13 03:45:10 +0900

    [Gold V] Title: 탑, Time: 444 ms, Memory: 120644 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const tops = input[1].split(' ').map(Number);
+const indexes = []; // 수신 가능한 탑의 번호 스택
+const results = []; // 수신한 탑의 번호 배열
+
+tops.forEach((current, index) => {
+  while (indexes.length > 0) {
+    const receivable = indexes[indexes.length - 1];
+
+    if (tops[receivable] > current) {
+      results.push(receivable + 1);
+      break;
+    } else {
+      indexes.pop();
+    }
+  }
+
+  if (indexes.length === 0) {
+    results.push(0);
+  }
+
+  indexes.push(index);
+});
+
+console.log(results.join(' '));
