59de64c3a5caa005f0f73f6b3e6badc0dd0c654c
경이 <devkyoung2@gmail.com>
2024-10-21 00:02:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 3632 ms, Memory: 62872 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,19 +1,11 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'Wiki\\input.txt';
 const [n, ...inputs] = fs.readFileSync(path, 'utf-8').trim().split('\n');
-const users = new Map();
 
-for (const input of inputs) {
-  const [age, name] = input.split(' ');
+const users = inputs.map((it) => it.split(' ')).sort((a, b) => +a[0] - +b[0]);
 
-  users.set(age, [...(users.get(age) ?? []), name]);
-}
+for (const user of users) {
+  const [age, name] = user;
 
-for (let i = 1; i <= 200; i++) {
-  const user = users.get(i.toString());
-
-  if (!user) continue;
-  for (const u of user) {
-    console.log(i, u);
-  }
+  console.log(age, name);
 }
