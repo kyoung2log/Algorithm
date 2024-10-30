c50d84d649c3108efd3d0fa369913cf8ea52cac3
경이 <devkyoung2@gmail.com>
2024-10-31 01:06:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 10628 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,29 +1,19 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const inputs = fs
+const [[n, k], ...items] = fs
   .readFileSync(path)
   .toString()
   .trim()
   .split('\n')
-  .map((it) => it.split(' ').map(Number));
-const [n, k] = inputs[0];
-const W = Array(n + 1).fill(0); // 무게
-const V = Array(n + 1).fill(0); // 가치
-const dp = Array.from({ length: n + 1 }, () => Array(k + 1).fill(0));
+  .map((line) => line.split(' ').map(Number));
 
-for (let i = 1; i <= n; i++) {
-  const [w, v] = inputs[i];
-  W[i] = w;
-  V[i] = v;
-}
+const dp = Array(k + 1).fill(0);
 
-for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= k; j++) {
-    const a = dp[i - 1][j];
-    const b = j - W[i] >= 0 ? dp[i - 1][j - W[i]] + V[i] : 0;
-
-    dp[i][j] = Math.max(a, b);
+for (let i = 0; i < n; i++) {
+  const [weight, value] = items[i];
+  for (let j = k; j >= weight; j--) {
+    dp[j] = Math.max(dp[j], dp[j - weight] + value);
   }
 }
 
-console.log(dp[n][k]);
+console.log(dp[k]);
