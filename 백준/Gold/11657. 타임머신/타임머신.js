863bfca12bacc31ecf07228fbaa2d56791a69ad8
경이 <devkyoung2@gmail.com>
2024-12-03 02:10:23 +0900

    [Gold IV] Title: 타임머신, Time: 244 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const [[n, k], ...inputs] = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((it) => it.split(' ').map(Number));
+
+const distance = Array(n + 1).fill(Infinity);
+
+distance[1] = 0;
+
+let hasNegativeCycle = false;
+for (let i = 1; i <= n; i++) {
+  for (const [a, b, c] of inputs) {
+    if (distance[a] !== Infinity && distance[b] > distance[a] + c) {
+      distance[b] = distance[a] + c;
+
+      if (i === n) hasNegativeCycle = true;
+    }
+  }
+}
+
+if (hasNegativeCycle) console.log(-1);
+else {
+  for (let i = 2; i <= n; i++) {
+    console.log(distance[i] === Infinity ? -1 : distance[i]);
+  }
+}
