e2038034975d81541f862f82c0bc1073a131bb42
경이 <devkyoung2@gmail.com>
2024-12-14 21:43:41 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 428 ms, Memory: 65520 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const [[v, e], ...inputs] = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((it) => it.split(' ').map(Number));
+const edges = inputs.sort((a, b) => a[2] - b[2]);
+const p = Array.from({ length: v + 1 }, (_, idx) => idx);
+
+const find = (a) => {
+  if (a !== p[a]) p[a] = find(p[a]);
+
+  return p[a];
+};
+
+const union = (a, b) => {
+  const pa = find(a);
+  const pb = find(b);
+
+  p[pb] = pa;
+};
+
+let ans = 0;
+for (const [s, e, c] of edges) {
+  const ps = find(s);
+  const pe = find(e);
+
+  if (ps === pe) continue;
+  union(s, e);
+  ans += c;
+}
+console.log(ans);
