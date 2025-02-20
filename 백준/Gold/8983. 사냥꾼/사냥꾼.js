4da9b8734553b080933addcc643649e8114b2757
경이 <devkyoung2@gmail.com>
2025-02-21 01:42:29 +0900

    [Gold IV] Title: 사냥꾼, Time: 428 ms, Memory: 62660 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.js
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.js
@@ -5,17 +5,42 @@ const [[m, n, l], place, ...animals] = fs
   .toString()
   .trim()
   .split('\n')
-  .map((it) => it.split(' ').map(Number));
+  .map((it, idx) => {
+    if (idx !== 1) return it.split(' ').map(Number);
+    return it
+      .split(' ')
+      .map(Number)
+      .sort((a, b) => a - b);
+  });
 let ans = 0;
+
+const binarySearch = (x) => {
+  let left = 0;
+  let right = place.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (place[mid] === x) return mid;
+    else if (place[mid] < x) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return left;
+};
 
 for (const [x, y] of animals) {
   if (y > l) continue;
 
-  for (let i = 0; i <= l - y; i++) {
-    if (place.includes(x + i) || place.includes(x - i)) {
-      ans += 1;
-      break;
-    }
+  // 가장 가까운 사대를 찾음
+  const idx = binarySearch(x);
+
+  if (idx < place.length && Math.abs(place[idx] - x) + y <= l) {
+    ans += 1;
+    continue;
+  }
+  if (idx > 0 && Math.abs(place[idx - 1] - x) + y <= l) {
+    ans += 1;
   }
 }
 
