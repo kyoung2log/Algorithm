b79bfdd5ec1da8c6fb7964badf715331860a4b93
경이 <devkyoung2@gmail.com>
2025-04-10 22:13:10 +0900

    [Gold III] Title: 색상환, Time: 188 ms, Memory: 19408 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.js
+++ 백준/Gold/2482. 색상환/색상환.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const [n, k] = fs.readFileSync(path).toString().trim().split('\n').map(Number);
 
+// 총 i개의 색이 있을 때, j개의 색을 인접하지 않게 고르는 경우
 const dp = Array.from({ length: n + 1 }, () => Array(k + 1).fill(0));
 const mod = 1000000003;
 
