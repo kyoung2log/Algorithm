c84b6299c2e5a616b0f167eb1b3e41612476595c
경이 <devkyoung2@gmail.com>
2025-04-11 15:06:32 +0900

    [Gold III] Title: 소수의 연속합, Time: 260 ms, Memory: 50316 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.js
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.js
@@ -1,34 +1,43 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const n = Number(fs.readFileSync(path).toString().trim());
-const number = Array(n + 1).fill(false);
-const primeNum = [];
+const n = +fs.readFileSync(path).toString().trim();
+const memo = Array(n + 1).fill(1);
+const primeNumber = [];
 
-for (let i = 2; i <= n; i++) {
-  if (number[i]) continue;
-  primeNum.push(i);
+// n까지의 모든 소수를 구한다
+for (let i = 2; i <= Math.sqrt(n); i++) {
+  if (memo[i] === 0) continue;
 
-  for (let j = i; j <= n; j += i) {
-    number[j] = true;
+  for (let j = i + i; j <= n; j += i) {
+    memo[j] = 0;
   }
 }
 
+for (let i = 2; i <= n; i++) {
+  if (memo[i]) primeNumber.push(i);
+}
+
+// 투포인터를 사용해 소수의 연속합의 개수를 카운팅한다.
 let left = 0;
-let right = 1;
-let ans = primeNum.includes(n) ? 1 : 0;
+let right = 0;
+let ans = 0;
+let sum = primeNumber[right];
 
-while (left < right && right < primeNum.length) {
-  let sum = 0;
-
-  for (let i = left; i <= right; i++) {
-    sum += primeNum[i];
-  }
-
-  if (sum > n) left += 1;
-  else if (sum < n) right += 1;
+while (left <= right && right < primeNumber.length) {
+  if (sum < n) {
+    right += 1;
+    sum += primeNumber[right];
+  } //
+  else if (sum > n) {
+    sum -= primeNumber[left];
+    left += 1;
+  } //
   else {
     ans += 1;
+    sum -= primeNumber[left];
     left += 1;
+    right += 1;
+    sum += primeNumber[right];
   }
 }
 
