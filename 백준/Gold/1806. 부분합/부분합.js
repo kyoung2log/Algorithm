9dafb3a8b12a5348c77c3dca8b5b0703e22e4d5e
경이 <devkyoung2@gmail.com>
2025-05-06 15:23:26 +0900

    [Gold IV] Title: 부분합, Time: 172 ms, Memory: 16768 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.js
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -10,27 +10,18 @@ const [n, s] = inputs[0];
 const sequence = inputs[1];
 
 let left = 0;
-let right = 1;
-let len = 2;
+let right = 0;
+let sum = sequence[left];
+
 let ans = Infinity;
-let sum = sequence[left] + sequence[right];
 
-while (left < right && right < n) {
-  if (sum >= s && ans > len) ans = len;
-
-  if (sum < s) {
-    sum += sequence[++right];
-    len += 1;
-  } //
-  else if (sum > s) {
-    sum -= sequence[left++];
-    len -= 1;
-  } //
+while (left <= right && right < n) {
+  if (sum < s) sum += sequence[++right];
   else {
+    ans = Math.min(ans, right - left + 1);
     sum -= sequence[left++];
-    sum += sequence[++right];
   }
 }
-if (sequence.filter((num) => num >= s).length > 0) console.log(1);
-else if (ans === Infinity) console.log(0);
+
+if (ans === Infinity) console.log(0);
 else console.log(ans);
