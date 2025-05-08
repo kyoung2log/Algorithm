626d709ebeeda3d86f856b0a49fae1c58278cc50
경이 <devkyoung2@gmail.com>
2025-05-08 23:15:17 +0900

    [Gold I] Title: 계단 수, Time: 220 ms, Memory: 25792 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.js
+++ 백준/Gold/1562. 계단 수/계단 수.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const n = Number(fs.readFileSync(path).toString().trim());
-
+const n = +fs.readFileSync(path).toString();
 const dp = Array.from({ length: n + 1 }, () => Array.from({ length: 10 }, () => Array(1 << 10).fill(0)));
 const mod = 1000000000;
 
@@ -10,24 +9,24 @@ for (let i = 1; i < 10; i++) {
 }
 
 for (let i = 2; i <= n; i++) {
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j <= 9; j++) {
     for (let k = 0; k < 1 << 10; k++) {
       if (j > 0) {
         dp[i][j][k | (1 << j)] += dp[i - 1][j - 1][k];
-        dp[i][j][k | (1 << j)] %= mod;
       }
+
       if (j < 9) {
         dp[i][j][k | (1 << j)] += dp[i - 1][j + 1][k];
-        dp[i][j][k | (1 << j)] %= mod;
       }
+
+      dp[i][j][k] %= mod;
     }
   }
 }
 
 let ans = 0;
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i <= 9; i++) {
   ans += dp[n][i][(1 << 10) - 1];
-  ans %= mod;
 }
 
-console.log(ans);
+console.log(ans % mod);
