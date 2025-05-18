5dc372bd206cd1640eeed0cbc57ad8c727420fad
경이 <devkyoung2@gmail.com>
2025-05-18 16:02:24 +0900

    [Gold III] Title: 게임 개발, Time: 204 ms, Memory: 15380 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.js
+++ 백준/Gold/1516. 게임 개발/게임 개발.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const [[n], ...inputs] = fs
+const inputs = fs
   .readFileSync(path)
   .toString()
   .trim()
   .split('\n')
   .map((it) => it.split(' ').map(Number));
+const [n] = inputs[0];
 const graph = Array.from({ length: n + 1 }, () => []);
 const indegree = Array(n + 1).fill(0);
 const costs = Array(n + 1).fill(0);
 const dp = Array(n + 1).fill(0);
 
-for (let i = 0; i < n; i++) {
-  costs[i + 1] = inputs[i].shift();
-  dp[i + 1] = costs[i + 1];
+// dp[n] = Math.max(dp[이전단계]) + cost[n]
+// 자기가 꼭 수행해야 하는 녀석들 + 자기 짓는 시간
+// 위의 dp가 수행되려면 이전단계(위상정렬) 순서대로 dp를 채워야 함
+
+for (let i = 1; i <= n; i++) {
+  dp[i] = costs[i] = inputs[i].shift();
+
+  inputs[i].pop();
+
   for (const num of inputs[i]) {
-    if (num === -1) break;
-    graph[num].push(i + 1);
-    indegree[i + 1] += 1;
+    graph[num].push(i);
+    indegree[i] += 1;
   }
 }
 
@@ -27,10 +33,6 @@ for (let i = 1; i <= n; i++) {
     q.push(i);
   }
 }
-
-// dp[n] = Math.max(dp[이전단계]) + cost[n]
-// 자기가 꼭 수행해야 하는 녀석들 + 자기 짓는 시간
-// 위의 dp가 수행되려면 이전단계(위상정렬) 순서대로 dp를 채워햐 함
 
 while (q.length) {
   const node = q.shift();
