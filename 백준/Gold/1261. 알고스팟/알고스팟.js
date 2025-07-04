2de892bfe921066f5062a018724b21fddf997895
경이 <devkyoung2@gmail.com>
2025-07-04 21:13:47 +0900

    [Gold IV] Title: 알고스팟, Time: 188 ms, Memory: 12560 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.js
+++ 백준/Gold/1261. 알고스팟/알고스팟.js
@@ -1,39 +1,42 @@
-const fs = require('fs');
-const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const inputs = fs.readFileSync(path).toString().trim().split('\n');
-const [m, n] = inputs.shift().split(' ').map(Number);
-const map = inputs.map((it) => it.split('').map(Number));
-const visited = Array.from({ length: n }, () => Array(m).fill(Infinity));
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const dx = [1, -1, 0, 0];
-const dy = [0, 0, 1, -1];
-const q = [[0, 0, 0]];
-let ans = Infinity;
+function solution(row, col, map) {
+  // 방문 처리(이동한 거리) + 벽을 부순 회수
+  const visited = Array.from({ length: col }, () => Array.from({ length: row }, () => false));
+  visited[0][0] = true;
 
-visited[0][0] = 0;
+  const queue = [[0, 0, 0]]; // 현재 X, Y 위치 + 벽을 부순 회수
 
-while (q.length) {
-  const [x, y, cnt] = q.shift();
-  
-  if (x === n - 1 && y === m - 1) ans = Math.min(ans, cnt);
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
 
-  for (let i = 0; i < 4; i++) {
-    const nx = x + dx[i];
-    const ny = y + dy[i];
+  // BFS 탐색
+  while (queue.length) {
+    const [currentX, currentY, broken] = queue.shift();
 
-    // 범위 체크
-    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+    if (currentX === col - 1 && currentY === row - 1) return broken;
 
-    if (map[nx][ny] === 0 && visited[nx][ny] > cnt) {
-      visited[nx][ny] = cnt;
-      q.push([nx, ny, cnt]);
-    }
-      
-    if (map[nx][ny] === 1 && visited[nx][ny] - 1 > cnt) {
-      visited[nx][ny] = cnt + 1;
-      q.push([nx, ny, cnt + 1]);
+    for (let i = 0; i < 4; i++) {
+      const [nextX, nextY] = [currentX + dx[i], currentY + dy[i]];
+
+      if (nextX < 0 || nextY < 0 || nextX >= col || nextY >= row) continue; // 지도를 벗어난 경우 생략
+      if (visited[nextX][nextY]) continue;
+
+      // 지도를 벗어나지 않은 경우
+      visited[nextX][nextY] = true;
+
+      if (!map[nextX][nextY]) queue.unshift([nextX, nextY, broken]);
+      else {
+        queue.push([nextX, nextY, broken + 1]);
+      }
     }
   }
 }
 
-console.log(ans);
+// M -> 가로 크기
+// N -> 세로 크기
+const [M, N] = input[0].split(" ").map(Number);
+const map = input.slice(1).map((element) => element.split("").map(Number));
+
+console.log(solution(M, N, map));
