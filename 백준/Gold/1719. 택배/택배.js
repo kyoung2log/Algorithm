554fe3806702c8adf55cbd0ec7294de235933a6d
경이 <devkyoung2@gmail.com>
2025-07-17 00:36:18 +0900

    [Gold III] Title: 택배, Time: 580 ms, Memory: 22516 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.js
+++ 백준/Gold/1719. 택배/택배.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-const [[n, m], ...inputs] = fs
+const inputs = fs
   .readFileSync(path)
   .toString()
   .trim()
   .split('\n')
   .map((it) => it.split(' ').map(Number));
+const [n, m] = inputs.shift();
+
 const graph = Array.from({ length: n + 1 }, () => []);
 const map = Array.from({ length: n + 1 }, () => Array(n + 1).fill('-'));
 
@@ -13,44 +15,40 @@ for (const [s, e, c] of inputs) {
   graph[s].push([e, c]);
   graph[e].push([s, c]);
 }
-
-const getMinNode = (distances, visited) => {
+const getMinNode = (visited, distances) => {
   let minNode = -1;
-  let minCost = Infinity;
+  let minDistance = Infinity;
 
   for (let i = 1; i <= n; i++) {
-    if (!visited[i] && distances[i] < minCost) {
-      minCost = distances[i];
+    if (!visited[i] && minDistance > distances[i]) {
       minNode = i;
+      minDistance = distances[i];
     }
   }
 
   return minNode;
 };
 
+// 다익스트라
 const dijkstra = (start) => {
   const distances = Array(n + 1).fill(Infinity);
   const visited = Array(n + 1).fill(false);
 
   distances[start] = 0;
 
-  for (let _ = 0; _ < n; _++) {
-    const node = getMinNode(distances, visited);
+  while (true) {
+    const node = getMinNode(visited, distances);
 
     if (node === -1) break;
     visited[node] = true;
 
     for (const [next, cost] of graph[node]) {
-      const distance = distances[node] + cost;
-
-      if (distances[next] > distance) {
+      if (distances[next] > distances[node] + cost) {
         distances[next] = distances[node] + cost;
         map[start][next] = map[start][node] !== '-' ? map[start][node] : next;
       }
     }
   }
-
-  return distances;
 };
 
 for (let i = 1; i <= n; i++) {
@@ -63,6 +61,5 @@ for (let i = 1; i <= n; i++) {
   for (let j = 1; j <= n; j++) {
     ans += map[i][j] + ' ';
   }
-
   console.log(ans.trim());
 }
