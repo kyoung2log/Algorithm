7ee5bbfd06735e70eff833612a57845173d5892f
경이 <devkyoung2@gmail.com>
2025-07-18 21:20:32 +0900

    [Silver II] Title: 랜선 자르기, Time: 172 ms, Memory: 13708 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -1,29 +1,25 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const inputs = fs.readFileSync(path).toString().trim().split('\n');
-const [n, k] = inputs[0].split(' ').map(Number);
-const lines = inputs
-  .slice(1)
-  .map(Number)
-  .sort((a, b) => a - b);
+const [k, n] = inputs.shift().split(' ').map(Number);
+const lines = inputs.map(Number).sort((a, b) => a - b);
 
 let left = 1;
 let right = lines.at(-1);
 let ans = 0;
-
 while (left <= right) {
   const mid = Math.floor((left + right) / 2);
+
   let cnt = 0;
 
   for (const line of lines) {
     cnt += Math.floor(line / mid);
   }
 
-  if (cnt < k) right = mid - 1;
-  else {
+  if (cnt >= n) {
     left = mid + 1;
     ans = mid;
-  }
+  } else right = mid - 1;
 }
 
 console.log(ans);
